420ba0ac06d19a1a9a16854e1fb90fff1e3d6388
arifstk <arifsk.dev@gmail.com>
2026-06-14 23:49:26 +0600

    Promise params

--- app/api/admin/orders/[id]/route.ts
+++ app/api/admin/orders/[id]/route.ts
@@ -1,14 +1,14 @@
 // app/api/admin/orders/[id]/route.ts
-// CHANGELOG: GET single order details by ID
 
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/db";
 import Order from "@/models/Order";
 
-export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(_: NextRequest, { params }: { params: Promise <{ id: string }> }) {
   try {
     await dbConnect();
-    const order = await Order.findById(params.id).lean();
+    const {id} = await params;
+    const order = await Order.findById(id).lean();
     if (!order) return NextResponse.json({ error: "Not found" }, { status: 404 });
     return NextResponse.json({ success: true, order });
   } catch (err: any) {
